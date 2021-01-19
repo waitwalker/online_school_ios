9c6d1e7213f26e921767ea87a4750deb6aeb9559
waitwalker <waitwalker@163.com>
2021-01-19 10:16:21 +0800

    Text相关属性学习

--- onlineSchool/ContentView.swift
+++ onlineSchool/ContentView.swift
@@ -5,11 +5,22 @@
 //  Created by waitwalker on 2021/1/19.
 //
 
+/// 教程https://github.com/Jinxiansen/SwiftUI/blob/master/README_CN.md
+/// 版权属于原作者
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        /// Text 用来展示一行或多行的文本内存,效果等同于UILabel,但更加优秀
+        /// 如果要创建Text,只需要通过Text("SwiftUI)即可创建;采用链式语法,也可以为文本添加多项属性,如字体,颜色,阴影,间距等
         Text("Hello, world!")
+            .bold()
+            .font(.system(size: 22))
+            .fontWeight(.medium)
+            .italic()
+            .shadow(color: .orange, radius: 1, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 2.0)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
             .padding()
     }
 }
