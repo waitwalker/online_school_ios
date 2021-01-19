fb99571d6a628f72997ad121cf92ba28659dde34
waitwalker <waitwalker@163.com>
2021-01-19 11:09:48 +0800

    Scene指定窗口&根控制器处理

--- onlineSchool-ios/SceneDelegate.swift
+++ onlineSchool-ios/SceneDelegate.swift
@@ -12,11 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
+    /// 和didFinish功能类似
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        if let windowScene = scene as? UIWindowScene {
+            let wind = UIWindow(windowScene: windowScene)
+            let launchController = LaunchAnimationViewController()
+            wind.rootViewController = launchController
+            self.window = wind
+            self.window?.makeKeyAndVisible()
+        }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- onlineSchool-ios/modules/launch/LaunchAnimationViewController.swift
+++ onlineSchool-ios/modules/launch/LaunchAnimationViewController.swift
@@ -12,7 +12,7 @@ class LaunchAnimationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .orange
     }
     
 
