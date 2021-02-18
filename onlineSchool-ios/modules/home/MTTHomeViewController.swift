eb6bdc3acc865ae6f02b7c859d2751d750a5f3d8
waitwalker <waitwalker@163.com>
2021-02-18 10:14:19 +0800

    首页智领卡片&智学卡片容器添加

--- onlineSchool-ios/modules/home/MTTHomeViewController.swift
+++ onlineSchool-ios/modules/home/MTTHomeViewController.swift
@@ -12,12 +12,9 @@ import NetworkLibrary
 
 class MTTHomeViewController: BaseViewController {
     
-    var zhiLingContainerView: UIView!
-    var collectionView: UICollectionView!
-    
-    let reusedId: String = "reusedId"
-    
-    var dataSource: [HomeCourseDataModel] = []
+    var zhiLingContainerView: MTTHomeCardContainerView!
+    var zhiLingDataSource: [HomeCourseDataModel] = []
+    var zhiXueDataSource: [HomeCourseDataModel] = []
     
     
 
@@ -37,21 +34,9 @@ class MTTHomeViewController: BaseViewController {
         
         let collectionViewHeight: CGFloat = itemHeight * 2 + navigationBarHeight + statusBarHeight + 20 + 15 + 10;
         
-        zhiLingContainerView = UIView(frame: CGRect(x: 0, y: sectionContainer.frame.maxY, width: self.view.bounds.size.width, height: collectionViewHeight))
+        zhiLingContainerView = MTTHomeCardContainerView(frame: CGRect(x: 0, y: sectionContainer.frame.maxY, width: self.view.bounds.size.width, height: collectionViewHeight))
         zhiLingContainerView.backgroundColor = UIColor(hex: "#3399ff")
         self.view.addSubview(zhiLingContainerView)
-        
-        let collectionViewFlowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        collectionViewFlowLayout.scrollDirection = .vertical
-        collectionViewFlowLayout.minimumInteritemSpacing = 15
-        collectionViewFlowLayout.minimumLineSpacing = 15
-        
-        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.w, height: collectionViewHeight), collectionViewLayout: collectionViewFlowLayout)
-        collectionView.backgroundColor = .white
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(MTTHomeCollectionViewCell.self, forCellWithReuseIdentifier: reusedId)
-        zhiLingContainerView.addSubview(collectionView)
         
         
         NetworkManager.sharedInstance.getRequest(ApiConst.home_course, parameters: nil) { (model) in
@@ -60,56 +45,16 @@ class MTTHomeViewController: BaseViewController {
             let homeModel: HomeCourseModel = model as! HomeCourseModel
             for(_, value) in homeModel.data.enumerated() {
                 if value.subjectId == 2 || value.subjectId == 3 || value.subjectId == 4 || value.subjectId == 5 {
-                    self.dataSource.append(value)
+                    self.zhiLingDataSource.append(value)
+                } else {
+                    self.zhiXueDataSource.append(value)
                 }
             }
-            self.collectionView.reloadData()
-            
+            self.zhiLingContainerView.dataSource = self.zhiLingDataSource
         }
         
     }
     
 
-}
-
-
-extension MTTHomeViewController: UICollectionViewDataSource {
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusedId, for: indexPath) as! MTTHomeCollectionViewCell
-        if dataSource.count > 0 {
-            cell.model = dataSource[indexPath.item]
-        }
-        return cell
-    }
-    
-    
-}
-
-extension MTTHomeViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-    }
-}
-
-extension MTTHomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width: CGFloat = (self.view.w - 20 * 2 - 20) / 2
-        let height: CGFloat = width * 2 / 3
-        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 10, left: 20, bottom: 0, right: 20)
-    }
 }
 
---
+++ onlineSchool-ios/modules/home/view/MTTHomeCardContainerView.swift
@@ -0,0 +1,91 @@
+//
+//  MTTHomeCardContainerView.swift
+//  onlineSchool-ios
+//
+//  Created by waitwalker on 2021/2/18.
+//
+
+import UIKit
+import NetworkLibrary
+
+/// 首页卡片容器 智领&智学课用到
+
+class MTTHomeCardContainerView: UIView {
+    
+    var collectionView: UICollectionView!
+    
+    let reusedId: String = "reusedId"
+
+    var dataSource: [HomeCourseDataModel]? {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let collectionViewFlowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionViewFlowLayout.scrollDirection = .vertical
+        collectionViewFlowLayout.minimumInteritemSpacing = 15
+        collectionViewFlowLayout.minimumLineSpacing = 15
+        
+        collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MTTHomeCollectionViewCell.self, forCellWithReuseIdentifier: reusedId)
+        self.addSubview(collectionView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
+
+extension MTTHomeCardContainerView: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reusedId, for: indexPath) as! MTTHomeCollectionViewCell
+        
+        if let data = self.dataSource {
+            cell.model = data[indexPath.item]
+        }
+        return cell
+    }
+    
+    
+}
+
+extension MTTHomeCardContainerView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension MTTHomeCardContainerView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width: CGFloat = (self.w - 20 * 2 - 20) / 2
+        let height: CGFloat = width * 2 / 3
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 20, bottom: 0, right: 20)
+    }
+}
+
+
