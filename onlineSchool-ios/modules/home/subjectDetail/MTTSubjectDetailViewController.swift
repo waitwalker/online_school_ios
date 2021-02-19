49c97e5f064169eaef3028f9c9b6087f7bd47bbd
waitwalker <waitwalker@163.com>
2021-02-19 08:32:50 +0800

    学科详情基类添加

---
+++ onlineSchool-ios/modules/home/subjectDetail/MTTSubjectDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MTTSubjectDetailViewController.swift
+//  onlineSchool-ios
+//
+//  Created by waitwalker on 2021/2/18.
+//
+
+import UIKit
+
+class MTTSubjectDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
